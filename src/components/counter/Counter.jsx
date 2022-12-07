c1052367cc1b54ddc1ef47127504eb26b42fb2b5
Marius <marius@example.com>
2022-12-07 09:00:03 +0200

    register form uzd

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
+
 function Counter(props) {
   // sukurti state counterValue
+  const [counterValue, setCounterValue] = useState(0);
 
   // sukurti fn handleUp, handleDown, handleUpBy, handleReset
-
+  function handleUp() {
+    // TODO: padaryti kodel reikia su anf atnaujinti pvz setTimeout
+    setCounterValue((prevCountValue) => prevCountValue + 1);
+  }
   // sudeti fn ant mygtuku kad veiktu aplikaicija
 
   return (
     <div className='card counter'>
       <p>Counter</p>
-      <h2>0</h2>
+      <h2>{counterValue}</h2>
       <div className='ctrl'>
-        <button>up</button>
+        <button onClick={handleUp}>up</button>
         <button>down</button>
         <button>up by 10</button>
         <button>reset</button>
