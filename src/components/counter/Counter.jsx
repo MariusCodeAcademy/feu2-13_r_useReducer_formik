4a180ed5e006365085df440443b62e76d5fceff4
Marius <marius@example.com>
2022-12-07 10:13:56 +0200

    clear out useState, add useReducer

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,33 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
 const initCounter = { value: 0 };
 
+function counterReducer(state, action) {}
+
 function Counter(props) {
   // sukurti state counterValue
-  const [counterState, setCounterState] = useState(initCounter);
+  // const [counterState, setCounterState] = useState(initCounter);
+  const [state, dispatch] = useReducer(counterReducer, initCounter);
 
   // sukurti fn handleUp, handleDown, handleUpBy, handleReset
   function handleUp() {
-    setCounterState((prevCountState) => ({ value: prevCountState.value + 1 }));
+    // setCounterState((prevCountState) => ({ value: prevCountState.value + 1 }));
     // setCounterValue(counterValue + 1); // not good
   }
   function handleDown() {
-    setCounterState((prevCountState) => ({ value: prevCountState.value - 1 }));
+    // setCounterState((prevCountState) => ({ value: prevCountState.value - 1 }));
   }
   function handleUpBy(howMuch) {
-    setCounterState((prevCountState) => ({
-      value: prevCountState.value + howMuch,
-    }));
+    // setCounterState((prevCountState) => ({
+    //   value: prevCountState.value + howMuch,
+    // }));
   }
   function handleReset() {
-    setCounterState(initCounter);
+    // setCounterState(initCounter);
   }
   // sudeti fn ant mygtuku kad veiktu aplikaicija
 
   return (
     <div className='card counter'>
       <p>Counter</p>
-      <h2>{counterState.value}</h2>
+      <h2>{state.value}</h2>
       <div className='ctrl'>
         <button onClick={handleUp}>up</button>
         <button onClick={handleDown}>down</button>
