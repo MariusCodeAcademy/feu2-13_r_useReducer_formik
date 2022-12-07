a1e1058e56a1b574e7c7c0c48f38fce4b76e677e
Marius <marius@example.com>
2022-12-07 09:30:39 +0200

    why update state with fn

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Counter(props) {
   // sukurti state counterValue
@@ -8,8 +8,18 @@ function Counter(props) {
   function handleUp() {
     // TODO: padaryti kodel reikia su anf atnaujinti pvz setTimeout
     setCounterValue((prevCountValue) => prevCountValue + 1);
+    // setCounterValue(counterValue + 1);
   }
   // sudeti fn ant mygtuku kad veiktu aplikaicija
+
+  useEffect(() => {
+    console.log('as pasileidziu kai tik baigia krautis komponentas');
+    setTimeout(() => {
+      setCounterValue((prevCountValue) => prevCountValue + 1);
+    }, 4000);
+  }, []);
+
+  console.log('as esu pries pat jsx');
 
   return (
     <div className='card counter'>
